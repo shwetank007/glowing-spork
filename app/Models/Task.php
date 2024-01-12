1da18365d04e56100c0b39368c06f11845e9b85c
shwetank007 <shwetank121339@gmail.com>
2024-01-12 11:08:16 +0530

    Feat: Creation of Note and Task Model

---
+++ app/Models/Task.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'tasks';
+    
+    protected $fillable = ['subject', 'description', 'start_date', 'due_date', 'status', 'priority'];
+
+    public function note() {
+        return $this->hasMany('App\Models\Note', 'task_id', 'id')->select(array('notes.id', 'notes.subject', 'notes.note', 'notes.attachment'));
+    }
+}
