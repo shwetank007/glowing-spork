7b9de22765480b872c06a0656a3a62ab5c250f29
shwetank007 <shwetank121339@gmail.com>
2024-01-12 14:05:50 +0530

    Fix: Task API Issue

--- Modules/Task/app/Http/Controllers/TaskController.php
+++ Modules/Task/app/Http/Controllers/TaskController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Response;
 use App\Models\Task;
 use App\Models\Note;
 use Validator;
+use Exception;
+use DB;
 
 class TaskController extends Controller
 {
@@ -23,6 +25,7 @@ class TaskController extends Controller
             'start_date' => 'required|date',
             'due_date' => 'required|date',
             'status' => 'required',
+            'priority' => 'required',
             'note' => 'required|array',
         ]);
         
@@ -34,40 +37,49 @@ class TaskController extends Controller
             ], 403);
         }
 
-        $task = new Task();
-        $task->subject = $request->subject;
-        $task->description = $request->description;
-        $task->start_date = $request->start_date;
-        $task->due_date = $request->due_date;
-        $task->status = $request->status;
-        $task->save();
+        
+        DB::beginTransaction();
+        try {
+            $task = new Task();
+            $task->subject = $request->subject;
+            $task->description = $request->description;
+            $task->start_date = $request->start_date;
+            $task->due_date = $request->due_date;
+            $task->status = $request->status;
+            $task->priority = $request->priority;
+            $task->save();
 
-
-        foreach($request->note as $note) {
-            $notes = new Note();
-            $notes->subject = $note["subject"];
-            $notes->note = $note["note"];
-            
-            $file = [];
-            if(!is_null($note["attachment"])) {
-                foreach($note["attachment"] as $attachment) {
-                    if(file_exists($attachment)) {
-                        $fileName = time().'_'.$attachment->getClientOriginalName();
-                        $filePath = $attachment->storeAs('uploads', $fileName, 'public');
-                        $file_path = '/storage/' . $filePath;
-                        $fileArray = [
-                            "file_name" => $fileName,
-                            "file_path" => $file_path
-                        ];
-                        array_push($file, $fileArray);
+            foreach($request->note as $note) {
+                $notes = new Note();
+                $notes->subject = $note["subject"];
+                $notes->note = $note["note"];
+                
+                $file = [];
+                if(!is_null($note["attachment"])) {
+                    foreach($note["attachment"] as $attachment) {
+                        if(file_exists($attachment)) {
+                            $fileName = time().'_'.$attachment->getClientOriginalName();
+                            $filePath = $attachment->storeAs('uploads', $fileName, 'public');
+                            $file_path = '/storage/' . $filePath;
+                            $fileArray = [
+                                "file_name" => $fileName,
+                                "file_path" => $file_path
+                            ];
+                            array_push($file, $fileArray);
+                        }
                     }
                 }
-            }
 
-            $notes->attachment = $file;
-            $notes->task_id = $task->id;
-            $notes->save();
+                $notes->attachment = $file;
+                $notes->task_id = $task->id;
+                $notes->save();
+            }
+        } catch (Exception $error) {
+            DB::rollBack();
         }
+
+        DB::commit();
+        
 
         $response = [
             'status' => 'success',
